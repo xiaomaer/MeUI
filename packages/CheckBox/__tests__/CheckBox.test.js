fdc45f3a702cb670853652ca7cc6493d3e9c15d9
mameiying <mameiying@beisen.com>
2020-03-19 14:56:28 +0800

    refactor: 测试单元测试jest

--- packages/CheckBox/__tests__/CheckBox.test.js
+++ packages/CheckBox/__tests__/CheckBox.test.js
@@ -1,34 +1,35 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Checkbox from '../src/index';
 
 // 测试组件的渲染
 test('render checkbox', () => {
-    const wrapper = shallow(
-        <Checkbox id="xiao" name="xxx" value="xxx">
-            label
-        </Checkbox>
-    );
-    expect(wrapper).toMatchSnapshot();
+  const wrapper = shallow(
+    <Checkbox id="xiao" name="xxx" value="xxx">
+      label
+    </Checkbox>,
+  );
+  expect(wrapper).toMatchSnapshot();
 });
 
 // 测试组件props
 test('render a checked props', () => {
-    const wrapper = shallow(
-        <Checkbox className="xm" id="xiao" name="xxx" value="xxx">
-            label
-        </Checkbox>
-    );
-    expect(wrapper.prop('className')).toEqual('xm');
+  const wrapper = shallow(
+    <Checkbox className="xm" id="xiao" name="xxx" value="xxx">
+      label
+    </Checkbox>,
+  );
+  expect(wrapper.prop('className')).toEqual('xm');
 });
 
 // 测试组件事件处理
 test('test onchange event', () => {
-    const wrapper = shallow(<Checkbox value="xxx">label</Checkbox>);
-    expect(wrapper).toMatchSnapshot();
-    wrapper.find('input').simulate('change', {
-        target: {
-            checked: true
-        }
-    });
-    expect(wrapper).toMatchSnapshot();
+  const wrapper = shallow(<Checkbox value="xxx">label</Checkbox>);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('input').simulate('change', {
+    target: {
+      checked: true,
+    },
+  });
+  expect(wrapper).toMatchSnapshot();
 });
